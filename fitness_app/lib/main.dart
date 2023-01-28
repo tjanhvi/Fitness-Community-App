8eb344d35c038e2580d484daa82fcf78ad66cbab
JANHVI TIWARI <tjanhvi2003@outlook.com>
2023-01-28 18:08:29 +0530

    Added discover section of homepage

--- fitness_app/lib/main.dart
+++ fitness_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'homepage_Sections/Discover.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,11 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Fitness App',      
+      home: const MyHomePage(title: 'Fitness App'),
     );
   }
 }
@@ -29,39 +28,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  int _selectedIndex = 0;
+  final ScrollController _homeController = ScrollController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        backgroundColor: Colors.purple.shade400,
+        actions: [          
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      body: Discover(),
+      bottomNavigationBar: BottomNavigationBar(        
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home', 
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_view_day),
+            label: 'Planner',
+          ), 
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Community',            
+          ),           
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+            
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedItemColor: Colors.purple.shade500,
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          switch (index) {
+            case 0:
+              if (_selectedIndex == index) {
+                _homeController.animateTo(
+                  0.0,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeOut,
+                );
+              }
+              break;
+            
+          }
+          setState(
+            () {
+              _selectedIndex = index;
+            },
+          );
+        },
+      )  
     );
   }
 }
