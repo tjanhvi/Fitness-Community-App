94f8839b496f8d42996f641971575ca644700c59
sudeshna <sudeshnadatta610@outlook.com>
2023-01-29 13:37:10 +0530

    added planner page

---
+++ fitness_app/lib/screens/planner/view.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:fitness_app/screens/planner/goal_datasource.dart';
+import 'package:fitness_app/screens/planner/goal_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class PlannerPage extends StatefulWidget {
+  const PlannerPage({Key? key}) : super(key: key);
+
+  @override
+  State<PlannerPage> createState() => _PlannerWidgetState();
+}
+
+class _PlannerWidgetState extends State<PlannerPage> {
+  CalendarView calendarView = CalendarView.day;
+  CalendarController calendarController = CalendarController();
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<GoalProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Planner'),
+        //backgroundColor: Colors.purple[800],
+        actions: [
+          IconButton(
+            onPressed: () {
+              provider.addGoals();
+            },
+            icon: const Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {
+              provider.editGoals(0);
+            },
+            icon: const Icon(Icons.edit),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      calendarView = CalendarView.day;
+                      calendarController.view = calendarView;
+                    });
+                  },
+                  child: Text("Day")),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      calendarView = CalendarView.week;
+                      calendarController.view = calendarView;
+                    });
+                  },
+                  child: Text("Week")),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      calendarView = CalendarView.month;
+                      calendarController.view = calendarView;
+                    });
+                  },
+                  child: Text("Month")),
+            ],
+          ),
+          Expanded(
+            child: SfCalendar(
+              view: calendarView,
+              controller: calendarController,
+              showNavigationArrow: true,
+              firstDayOfWeek: 1,
+              todayHighlightColor: Colors.purple[400],
+              selectionDecoration: BoxDecoration(
+                  border:
+                      Border.all(color: Color.fromARGB(255, 161, 122, 168))),
+              dataSource: GoalDataSource(provider.goals),
+              monthViewSettings: MonthViewSettings(
+                appointmentDisplayMode: MonthAppointmentDisplayMode.indicator,
+                showAgenda: true,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
